48b7b85d9caa739186b495d0a04cbd958539ea66
matt <mattnestler96@gmail.com>
2022-10-27 11:22:50 -0700

    add demographic data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { findIncomeByZip, findHomeValueByZip, findRentValueByZip, ZillowResponse } from './api'
+import { findIncomeByZip, findHomeValueByZip, findRentValueByZip, findDemoDataByZip, ZillowResponse } from './api'
 import { Amplify, Geo } from 'aws-amplify';
 import { Place } from '@aws-amplify/geo'
 import awsconfig from './aws-exports';
@@ -18,14 +18,15 @@ const currency = (value: number | string): string => {
 
 function App() {
 
-  const [initialViewState, setInitialViewState] = React.useState<ViewState | undefined>()
+  const [initialViewState, setInitialViewState] = React.useState<Partial<ViewState> | undefined>()
   const getInitialLocation = () => {
     navigator.geolocation.getCurrentPosition(function(position) {
       const {latitude, longitude} = position.coords
       const viewState = {latitude, longitude, zoom: 9} as ViewState
       setInitialViewState(viewState)
       handleMove({ viewState })
-    });
+    },
+    () => setInitialViewState({}));
   }
   React.useEffect(getInitialLocation, [])
 
@@ -33,12 +34,14 @@ function App() {
   const [incomeData, setIncomeData] = React.useState<string | undefined>()
   const [rentData, setRentData] = React.useState<ZillowResponse | undefined>();
   const [homeData, setHomeData] = React.useState<ZillowResponse | undefined>();
+  const [demoData, setDemoData] = React.useState<any | undefined>();
   const findZip = () => {
     const zip = currentLocation?.postalCode?.substring(0,5) || ''
     if (zip) {
       findIncomeByZip(`${zip}`).then(setIncomeData)
       findHomeValueByZip(`${zip}`).then(setHomeData)
       findRentValueByZip(`${zip}`).then(setRentData)
+      findDemoDataByZip(`${zip}`).then(setDemoData)
     }
   }
   React.useEffect(findZip, [currentLocation])
@@ -59,9 +62,22 @@ function App() {
               <h5>{currentLocation.label}</h5>
             </>
           )}
-          {incomeData && <p>Average Household Income {currency(incomeData)}</p>}
-          {homeData && <p>Average Home Value {currency(homeData.Value)}</p>}
-          {rentData && <p>Average Rent {currency(rentData.Value)}</p>}
+          <div style={{ display: 'flex', flexDirection: 'row'}}>
+            <div style={{ margin: 10 }}>
+              {incomeData && <p>Average Household Income {currency(incomeData)}</p>}
+              {homeData && <p>Average Home Value {currency(homeData.Value)}</p>}
+              {rentData && <p>Average Rent {currency(rentData.Value)}</p>}
+            </div>
+            <div style={{ margin: 10 }}>
+              {demoData && (
+                <>
+                  {Object.entries(demoData).map(([key, value]) => (
+                    <p>{`${key} - ${value}`}</p>
+                  ))}
+                </>
+              )}
+            </div>
+          </div>
           {initialViewState && 
             <MapView
               reuseMaps={true}
--- src/api.ts
+++ src/api.ts
@@ -16,10 +16,29 @@ export type ZillowResponse = {
 }
 
 export const findIncomeByZip = async (zip: string): Promise<any> => {
-    // api.census.gov/data/2020/acs/acs5?get=NAME,group(B01001)&for=us:1&key=YOUR_KEY_GOES_HERE
     const {data} = await axios.get(`https://api.census.gov/data/2020/acs/acs5?get=NAME,group(B19013)&for=zip%20code%20tabulation%20area:${zip}&key=${CENSUS_KEY}`)
     const [rawKeys, rawData] = data
     return rawData[rawKeys.findIndex((d: string) => d === 'B19013_001E')]
+}
+
+const demoDataKeys: Record<string, string> = {
+    'B01001_001E': 'totalPopulation',
+    'B01001A_001E': 'whiteTotalPopulation',
+    'B01001B_001E': 'blackTotalPopulation',
+    'B01001C_001E': 'nativeTotalPopulation',
+    'B01001E_001E': 'islanderTotalPopulation',
+    'B01001F_001E': 'otherTotalPopulation',
+    'B01001H_001E': 'nonHispanicTotalPopulation',
+    'B01001I_001E': 'hispanicTotalPopulation'
+}
+export const findDemoDataByZip = async (zip: string): Promise<any> => {
+    const lookupKeys = Object.keys(demoDataKeys)
+    const {data} = await axios.get(`https://api.census.gov/data/2020/acs/acs5?get=NAME,${lookupKeys}&for=zip%20code%20tabulation%20area:${zip}&key=${CENSUS_KEY}`)
+    const [rawKeys, rawData] = data
+    const entries = rawKeys
+                        .map((v: string, k: string) => ([demoDataKeys[v] || 'OUT', rawData[k]]))
+                        .filter(([key]: string[]) => key !== 'OUT')
+    return Object.fromEntries(entries)
 }
 
 export const findHomeValueByZip = async (zip: string): Promise<ZillowResponse> => {
